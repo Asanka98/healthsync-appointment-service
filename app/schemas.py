456e397f780c4957e13232390bae5dfbb3bfa355
asanka98 <asanka.asb12345.com>
2025-01-07 14:13:59 +0530

    Deployment

--- app/schemas.py
+++ app/schemas.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Union
 
 class AppointmentBase(BaseModel):
     patient_name: str
     doctor_name: str
     appointment_time: datetime
-    reason: str 
+    reason: Union[str, None] = None 
 
 class AppointmentCreate(AppointmentBase):
     pass
